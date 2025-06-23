dcd4b9c0c74d54cd4ba7cfff20d8fd64a0484398
Jinly39 <j.muangsiri24@gmail.com>
2025-06-23 08:25:05 +0000

    save

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,12 @@
+num1_str = input ("please type the first number: ")
+num1 = float(num1_str)
+num2_str +input ("please type the secomd number: ")
+num2=float(num2_str)
+plus = num1 + num2 
+minus = num1-num2
+multiply = num1 * num2
+divide = num1 / num2
+print (num1, "+", num2, "=", plus)
+print (num1, "-", num2, "=", minus)
+print (num1, "*", num2, "=", multiplying)
+print (num1, "/", num2, "=", divide)
