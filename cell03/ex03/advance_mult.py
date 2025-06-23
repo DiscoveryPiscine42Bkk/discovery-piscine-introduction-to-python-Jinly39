171ab9e4600868e2b7c626e2896e269718564eb2
Jinly39 <j.muangsiri24@gmail.com>
2025-06-23 04:47:53 +0000

    save

---
+++ cell03/ex03/advance_mult.py
@@ -0,0 +1,11 @@
+i = 0
+while i <= 10 :
+    print (f"table de {i}:", end=" ")
+
+    j=0
+    while j<= 10:
+        print(i * j, end = " ")
+        j += 1
+
+    print() #newline after each table
+    i += 1                                                                    
