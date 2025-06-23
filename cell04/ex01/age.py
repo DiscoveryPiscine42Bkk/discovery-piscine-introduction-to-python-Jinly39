eb6957987b7f7aff9385344ef9b4df4315f8434b
Jinly39 <j.muangsiri24@gmail.com>
2025-06-23 05:10:53 +0000

    save

---
+++ cell04/ex01/age.py
@@ -0,0 +1,8 @@
+#ask the user for their age
+age = input ("please enter your age: ")
+#convert input string to an integer 
+age= int (age)
+print ("you are currently {} years old". format(age))
+print("in 10 years, you'll be {} years old.". format (age+10))
+print("in 20 years, you'll be{} years old.". format (age+20))
+print ("in 30 years, you'll be {} years old.". format (age+30))
